acfe351eeeaab3022a135489ee05fe857a07f266
Hans Fangohr <hans.fangohr@xfel.eu>
2022-01-27 21:59:13 +0100

    added more comments

--- wordle.py
+++ wordle.py
@@ -21,20 +21,52 @@ def get_word_list():
 
 words = get_word_list()
 
+# select only those words that are 5 characters long
 w5 = [word for word in words if len(word) == 5]
 
-# letters with known correct positions
+# enter letters with known correct positions here:
+# for 'alife', we would type
+#
+# correct_pos = {"a": 0, "l": 1, "i": 2, "f": 3, "e": 4}
+#
+# if we don't know any correct letters with correct positions yet, use
+#
+# correct_pos = {}
 
-correct_pos = {"c": 0, "r": 1, "i": 2, "p": 4}
-# correct_pos = {'e': 4, 'i': 1, 'n': 2, 'c':3}
+correct_pos = {}
 
-# known incorrect letters
-incorrect_letters = set("alveunons")
+# Add letters which we know are wrong and do not occur in the searched
+# worlde. These can be added in any order. For example, if
+# the letters x, y, z, a, k, and c are incorrect, we could write:
+# incorrect_letters = set("xyzakc")
+#
+# start with an empty set:
+# incorrect_letters = set("")
 
-# correct letters with known incorrect position
-incorrect_position = {"r": 3}
+incorrect_letters = set("")
+
+# Add letters for which we know that they occur in the worlde, but
+# we haven't got the correct position yet. Enter the wrong position as
+# an index.
+#
+# For example: if 'm' is in the word but not in the first position
+# (=index 0), and  'q' is in the word but not in the third
+# position (=index 2), write:
+#
+# incorrect_position = {'m': 0, 'q':2}
+#
+# start with empty dictionary:
+#
+# incorrect_position = {}
+
+incorrect_position = {}
+
 must_include = set(incorrect_position.keys())
 
+
+# No changes needed below here
+#
+# ==========================================
 
 sel = w5
 print(f"Found {len(sel)} words with 5 characters.")
